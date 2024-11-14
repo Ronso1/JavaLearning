979d570a8de83dddaaa28ea1070b7af6cac601ed
Ivan <gureff.al@yandex.ru>
2024-11-14 16:16:04 +0300

    Init repo

---
+++ src/InputOutput.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        doubleRofl();
+    }
+
+    public static void testNumbers() {
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 15;
+        int num4 = 10;
+        int num5 = 5;
+        int result = 12;
+
+        result += num1 * num2 + num3 % num4 / num5;
+        System.out.println(result);
+    }
+
+    public static void inputOutputTest() {
+        Scanner input = new Scanner(System.in);
+        float a = input.nextFloat();
+
+        System.out.println(a);
+    }
+
+    public static void doubleRofl() {
+        double x = 8.8;
+        double y = 1.5;
+        double z = x - y;
+        System.out.println(z);
+    }
+}
