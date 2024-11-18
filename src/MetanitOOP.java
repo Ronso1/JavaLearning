ceae8986a42949e035bf2f3432e9c36f8dd3522d
Ivan <gureff.al@yandex.ru>
2024-11-18 23:39:23 +0300

    Add OOP example

--- src/MetanitOOP.java
+++ src/MetanitOOP.java
@@ -1,5 +1,10 @@
 public class MetanitOOP {
     public static void main(String[] args) {
-        
+        People people = new People("Ivan", 23);
+        People people1 = new People();
+        people.showInfo();
+        System.out.println();
+        people1.showInfo();
     }
 }
+
---
+++ src/People.java
@@ -0,0 +1,18 @@
+public class People {
+    String name = "Undentified";
+    int age = 0;
+
+    public People() {
+
+    }
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void showInfo() {
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+    }
+}
